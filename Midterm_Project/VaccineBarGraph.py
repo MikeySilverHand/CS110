7a2640b0121a8d4761b59aed7ddb389ae07bc10d
MikeySilverHand <herna2048@csusm.edu>
2025-09-22 23:58:14 -0700

    Update Vaccine.py

--- Midterm_Project/VaccineBarGraph.py
+++ Midterm_Project/VaccineBarGraph.py
@@ -1,15 +1,31 @@
+#------------------------------------------------------------
+# Hypotheis: 
+#
+#------------------------------------------------------------
+
 import matplotlib.pylab as plt
-from matplotlib import cm
-import math
+#from matplotlib import cm
+#import math
 import pandas as pd
 import numpy as np
-import random
+import seaborn as sns
 
-plt.rc('font', family='times')
-plt.rc('xtick', labelsize=10)
-plt.rc('ytick', labelsize=10)
-plt.rc('font', size=12)
-#%matplotlib inline #Depends on coding IDE!
-data = pd.read_csv("Midterm_Project/country_vaccinations.csv", encoding='latin-1')
+sns.set_theme()
+#import random
 
-print(data.columns)
+# ------------------------------------------------------------
+# mv = manufacturer of vacanations
+# wdd = day wise
+# fg = full grouped
+# ------------------------------------------------------------
+mv = pd.read_csv('Midterm_Project/country_vaccinations_by_manufacturer.csv') # manufacturer of vacanations and how much is given daily
+wdd = pd.read_csv('Midterm_Project/worldometer_data.csv') # recovered, confirmed, deaths day wise
+# fg = pd.read_csv('Midterm_Project/full_grouped.csv') # cases by country (recovered, confirmed, deaths)
+
+
+wdd['Coutry/Region'] = 'Country/Region'
+death_count = wdd.groupby('TotalDeaths')['TotalCases']
+
+g = sns.lmplot(data=wdd, x="TotalCases", y="TotalDeaths", hue="Country/Region", height=5)
+g.set_axis_labels("Total Cases Recoreded millions", "Total Deaths recorded thousands")
+plt.show()
