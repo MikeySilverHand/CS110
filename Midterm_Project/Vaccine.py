7dc12ea0bf7e18af583de86aff11104566b26f2d
Michael <herna2048@csusm.edu>
2025-09-27 19:02:20 -0700

    Added to project

--- Midterm_Project/Vaccine.py
+++ Midterm_Project/Vaccine.py
@@ -4,37 +4,41 @@
 #------------------------------------------------------------ 
 
 import matplotlib.pylab as plt 
-
-#from matplotlib import cm 
-#import math 
 import pandas as pd 
 import numpy as np 
 import seaborn as sns 
- 
-sns.set_theme() 
 import random 
+
+sns.set_theme() 
 
 # ------------------------------------------------------------ 
 # mv = manufacturer of vacanations 
-# wdd = day wise 
-# fg = full grouped 
+# wom = worldometer 
+# gdp = gross domestic product
 # ------------------------------------------------------------ 
 
 mv = pd.read_csv('Midterm_Project/country_vaccinations_by_manufacturer.csv') # manufacturer of vacanations and how much is given daily 
-wdd = pd.read_csv('Midterm_Project/worldometer_data.csv') # recovered, confirmed, deaths day wise 
-gdp = pd.read_csv('Midterm_Project/') # Each countries GPD from 2005-2025
+wom = pd.read_csv('Midterm_Project/worldometer_data.csv') # recovered, confirmed, deaths day wise 
+gdp = pd.read_csv('Midterm_Project/GDP.csv') # Each countries GPD from 2005-2025
 
-Africa = wdd.loc[wdd['WHO Region']=='Africa']
-North_America = wdd.loc[wdd['Country/Region']=='North America']
-South_America = wdd.loc[wdd['Country/Region']=='South America']
-Eastern_Mediterranean = wdd.loc[wdd['WHO Region']=='EasternMediterranean']
-Europe = wdd.loc[wdd['WHO Region']=='Europe']
-South_East_Asia = wdd.loc[wdd['WHO Region']=='South-EastAsia']
-Western_Pacific = wdd.loc[wdd['WHO Region']=='WesternPacific']
+#All of the countries that have information
+countries = wom['Country/Region'].unique()
 
-Regions = [Africa, North_America, South_America, Eastern_Mediterranean, Europe, South_East_Asia, Western_Pacific]
+#Limit the data to only use 2020-2022 and make sure all the countries match up
+gdp_filtered = gdp.loc[gdp['Year'].isin([2020, 2021, 2022]) & (gdp['Country Name'].isin(countries))]
 
-for region in Regions:
-  print(region)
+#see how many times each country is referred to each year
+counts = gdp_filtered.groupby('Country Name')['Year'].nunique()
 
-death_count = wdd.groupby('Deaths/1M pop')['Tot Cases/1M pop'] 
+#find the valid countries that are referred to 3 times for each year
+valid_countries = counts[counts == 3].index
+
+#filter it so that only countries that are considered "valid" will be kept
+years = gdp_filtered[gdp_filtered['Country Name'].isin(valid_countries)]
+
+countries_ordered = years.groupby('Country Name')['Value'].sum()
+
+#store the GDP's of each country for each year
+gdp_2020 = np.sort(years[(years['Year'] == 2020)]['Value'])
+gdp_2021 = np.sort(years[(years['Year'] == 2021)]['Value'])
+gdp_2022 = np.sort(years[(years['Year'] == 2022)]['Value'])
