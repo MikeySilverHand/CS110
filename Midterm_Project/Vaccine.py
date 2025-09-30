9802e3a51d9d2e003a02f3be164495316fd5da30
Michael <herna2048@csusm.edu>
2025-09-30 00:48:03 -0700

    added a lot to vaccine

--- Midterm_Project/Vaccine.py
+++ Midterm_Project/Vaccine.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np 
 import seaborn as sns 
+import math
 import random 
 
 sns.set_theme() 
@@ -22,27 +23,57 @@ wom = pd.read_csv('worldometer_data.csv') # recovered, confirmed, deaths day wis
 gdp = pd.read_csv('GDP.csv') # Each countries GPD from 2005-2025
 
 #All of the countries that have information
-countries = wom['Country/Region'].unique()
+max_countries_one = wom['Country/Region'].unique()
+max_countries_two = mv['location'].unique()
+
+#Group By GDP
 
 #Limit the data to only use 2020-2022 and make sure all the countries match up
-gdp_filtered = gdp.loc[gdp['Year'].isin([2020, 2021, 2022]) & (gdp['Country Name'].isin(countries))]
+gdp_unfiltered_countries = gdp.loc[gdp['Year'].isin([2020, 2021, 2022]) & (gdp['Country Name'].isin(max_countries_one)) & (gdp['Country Name'].isin(max_countries_two))]
 
 #see how many times each country is referred to each year
-counts = gdp_filtered.groupby('Country Name')['Year'].nunique()
+counts = gdp_unfiltered_countries.groupby('Country Name')['Year'].nunique()
 
 #find the valid countries that are referred to 3 times for each year
 valid_countries = counts[counts == 3].index
 
 #filter it so that only countries that are considered "valid" will be kept
-years = gdp_filtered[gdp_filtered['Country Name'].isin(valid_countries)]
+gdp_filtered_countries = gdp_unfiltered_countries[gdp_unfiltered_countries['Country Name'].isin(valid_countries)]
 
-countries_names = years['Country Name'].unique()
+#Store everything in a data set with one of each country and their GDP
+countries = gdp_filtered_countries.groupby('Country Name')['Value'].sum().reset_index()
 
-sums = years.groupby('Country Name')['Value'].sum()
+#Group By Total Vaccinations
 
-#store the GDP's of each country for each year
-gdp_2020 = np.sort(years[(years['Year'] == 2020)]['Value'])
-gdp_2021 = np.sort(years[(years['Year'] == 2021)]['Value'])
-gdp_2022 = np.sort(years[(years['Year'] == 2022)]['Value'])
+#Filter vaccinations
+vaccinations_filtered_countries = mv[mv['location'].isin(valid_countries)]
 
-gdp_sums = years.groupby(years['Country Name'].unique())[years.groupby('Country Name')['Value'].sum()]
+#Store everything in a data set\
+sum_vaccinations = vaccinations_filtered_countries.groupby('location')['total_vaccinations'].sum().reset_index().sort_values(by='location').reset_index(drop=True)
+countries['Vaccinations'] = sum_vaccinations['total_vaccinations']
+
+#Group By Total cases and deaths by 1 million pop
+
+#Filter total cases
+total_cases_n_deaths_filtered_countries = wom[wom['Country/Region'].isin(valid_countries)]
+
+#Store everything in a data set
+total_cases = total_cases_n_deaths_filtered_countries.groupby('Country/Region')['Tot Cases/1M pop'].sum().reset_index().sort_values(by='Country/Region').reset_index(drop=True)
+countries['Cases'] = total_cases['Tot Cases/1M pop']
+total_deaths = total_cases_n_deaths_filtered_countries.groupby('Country/Region')['Deaths/1M pop'].sum().reset_index().sort_values(by='Country/Region').reset_index(drop=True)
+countries['Deaths'] = total_deaths['Deaths/1M pop']
+
+#order the countries by GDP
+countries = countries.reset_index().sort_values(by='Value').reset_index(drop=True)
+
+#group it into 16 different groups
+n = len(countries)
+c = math.ceil((n-1)/10)
+
+groups = [[0 for i in range(math.ceil((n-1)/10))] for j in range(9)]
+
+num = 0
+for i in range(c):
+    for j in range(3):
+        groups[j][i] = countries[num]
+        num+=1
